309c691e4620e5895f0d0cbc090b4e4a3701a916
sascha-raesch <sascha.raesch@threedy.io>
2024-05-02 15:54:47 +0200

    added: initial stub code

---
+++ step-merger/src/error.rs
@@ -0,0 +1,26 @@
+use quick_error::quick_error;
+use std::io;
+
+quick_error! {
+    #[derive(Debug)]
+    pub enum Error {
+        IO(err: String) {
+            display("{}", err)
+        }
+        Internal(err: String) {
+            display("{}", err)
+        }
+        InvalidFormat(err: String) {
+            display("{}", err)
+        }
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(error: io::Error) -> Self {
+        Error::IO(format!("{}", error))
+    }
+}
+
+/// The result type used in this crate.
+pub type Result<T> = std::result::Result<T, Error>;
