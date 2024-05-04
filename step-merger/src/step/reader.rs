379e2331eb879bf619f6b62414d91ff2b2b5d19f
sascha-raesch <sascha.raesch@threedy.io>
2024-05-04 14:13:58 +0200

    added: full first code for parsing the STEP file

--- step-merger/src/step/reader.rs
+++ step-merger/src/step/reader.rs
@@ -110,20 +110,16 @@ fn parser() -> impl Parser<char, ParsedStep, Error = Simple<char>> {
         let comment = just("/*").then(take_until(just("*/"))).padded();
         let ignore = comment.repeated();
 
-        // The parser for the initial ISO string.
-        let iso = just("ISO-")
-            .ignore_then(filter(|c| *c != ';').repeated())
-            .then_ignore(just(';'))
-            .collect::<String>()
-            .padded()
-            .padded_by(ignore);
-
         let str = just('\'')
             .ignore_then(filter(|c| *c != '\'').repeated())
             .then_ignore(just('\''))
             .collect::<String>()
             .padded()
             .padded_by(ignore);
+
+        let int = text::int(10)
+            .map(|s: String| s.parse::<u64>().unwrap())
+            .padded();
 
         // The parser for a list of strings in brackets, i.e. ('Foobar') or ('adasd', 'asdasd').
         let str_brackets = str
@@ -132,6 +128,14 @@ fn parser() -> impl Parser<char, ParsedStep, Error = Simple<char>> {
             .flatten()
             .delimited_by(just('('), just(')'))
             .labelled("array");
+
+        // The parser for the initial ISO string.
+        let iso = just("ISO-")
+            .ignore_then(filter(|c| *c != ';').repeated())
+            .then_ignore(just(';'))
+            .collect::<String>()
+            .padded()
+            .padded_by(ignore);
 
         // The parser for the file description.
         let file_description = text::keyword("FILE_DESCRIPTION")
@@ -214,7 +218,77 @@ fn parser() -> impl Parser<char, ParsedStep, Error = Simple<char>> {
             .padded_by(ignore)
             .labelled("header");
 
-        header
+        // The parser for the content of a STEP entry.
+        let step_entry_content = filter(|c| *c != ';')
+            .repeated()
+            .collect::<String>()
+            .then_ignore(just(';'))
+            .padded()
+            .padded_by(ignore);
+
+        // The parser for a STEP entry id.
+        let step_entry_id = just('#')
+            .padded()
+            .padded_by(ignore)
+            .ignore_then(int)
+            .padded()
+            .padded_by(ignore);
+
+        // The parser for a STEP entry.
+        let step_entry = step_entry_id
+            .then_ignore(just('='))
+            .then(step_entry_content)
+            .padded()
+            .padded_by(ignore)
+            .map(|(id, content)| StepEntry::new(id, &content.to_owned()))
+            .labelled("step_entry");
+
+        let step_entries = step_entry
+            .chain(step_entry.repeated())
+            .or_not()
+            .flatten()
+            .map(ParsedStep::Data)
+            .labelled("step_entries");
+
+        // The parser for the data section of a STEP file.
+        let data = text::keyword("DATA")
+            .padded()
+            .padded_by(ignore)
+            .ignore_then(just(';'))
+            .ignore_then(step_entries)
+            .padded()
+            .padded_by(ignore)
+            .then_ignore(text::keyword("ENDSEC"))
+            .padded()
+            .padded_by(ignore)
+            .then_ignore(just(';'))
+            .labelled("data");
+
+        // The parser for the full STEP file.
+        let step = header
+            .then(data)
+            .map(|(header, data)| {
+                // extract the header from the data
+                let header = if let ParsedStep::Header(header) = header {
+                    header
+                } else {
+                    unreachable!()
+                };
+
+                // extract the data from the data
+                let data = if let ParsedStep::Data(data) = data {
+                    data
+                } else {
+                    unreachable!()
+                };
+
+                ParsedStep::Step(header, data)
+            })
+            .padded()
+            .padded_by(ignore)
+            .labelled("step");
+
+        step
     })
 }
 
@@ -225,26 +299,58 @@ mod tests {
     #[test]
     fn test_reading() {
         let data = r#"
-
         ISO-10303-21;
+
         HEADER;
-        FILE_DESCRIPTION(('CTC-02 geometry with PMI representation and/or presentation','from the NIST MBE PMI Validation and Conformance Testing Project'),'2;1');
-        FILE_NAME('nist_ctc_02_asme1_ap203.stp','2017-03-10T12:15:07-07:00',(''),(''),'','','');
-        FILE_SCHEMA (('AP203_CONFIGURATION_CONTROLLED_3D_DESIGN_OF_MECHANICAL_PARTS_AND_ASSEMBLIES_MIM_LF { 1 0 10303 403 2 1 2}'));
+            FILE_DESCRIPTION(('CTC-02 geometry with PMI representation and/or presentation','from the NIST MBE PMI Validation and Conformance Testing Project'),'2;1');
+            FILE_NAME('nist_ctc_02_asme1_ap203.stp','2017-03-10T12:15:07-07:00',(''),(''),'','','');
+            FILE_SCHEMA (('AP203_CONFIGURATION_CONTROLLED_3D_DESIGN_OF_MECHANICAL_PARTS_AND_ASSEMBLIES_MIM_LF { 1 0 10303 403 2 1 2}'));
         ENDSEC;
 
-        "#;
+        DATA;
+        #10=CONSTRUCTIVE_GEOMETRY_REPRESENTATION_RELATIONSHIP('supplemental geometry','',#376,#11);
+        #11=CONSTRUCTIVE_GEOMETRY_REPRESENTATION('supplemental geometry',(#10644,#10645,#10646,#10647,#10648),#46150);
+        #12=GEOMETRICALLY_BOUNDED_WIREFRAME_SHAPE_REPRESENTATION('nist_ctc_02_asme1-None',(#10532),#46150);
+        #13=PROPERTY_DEFINITION_REPRESENTATION(#17,#15);
+        #14=PROPERTY_DEFINITION_REPRESENTATION(#18,#16);
+        #15=REPRESENTATION('',(#19),#46150);
+        #16=REPRESENTATION('',(#20),#46150);
+        ENDSEC;
+
+        END-ISO-10303-21;
+            "#;
 
         let result = parser().parse(data);
         assert!(result.is_ok(), "Failed with {:?}", result);
         let parsed_step = result.unwrap();
-        assert_eq!(
-            parsed_step,
-            ParsedStep::Header(StepHeader{
-                iso: "10303-21".to_owned(),
-                implementation_level: "2;1".to_owned(),
-                protocol: vec!["AP203_CONFIGURATION_CONTROLLED_3D_DESIGN_OF_MECHANICAL_PARTS_AND_ASSEMBLIES_MIM_LF { 1 0 10303 403 2 1 2}".to_owned()],
-            })
-        );
+
+        if let ParsedStep::Step(header, body) = parsed_step {
+            assert_eq!(header.iso, "10303-21");
+            assert_eq!(header.implementation_level, "2;1");
+            assert_eq!(header.protocol.len(), 1);
+            assert_eq!(header.protocol[0], "AP203_CONFIGURATION_CONTROLLED_3D_DESIGN_OF_MECHANICAL_PARTS_AND_ASSEMBLIES_MIM_LF { 1 0 10303 403 2 1 2}");
+
+            assert_eq!(body.len(), 7);
+
+            for (entry, id) in body.iter().zip(10..) {
+                assert_eq!(entry.get_id(), id);
+            }
+
+            assert_eq!(body[0].get_definition(), "CONSTRUCTIVE_GEOMETRY_REPRESENTATION_RELATIONSHIP('supplemental geometry','',#376,#11)");
+            assert_eq!(body[1].get_definition(), "CONSTRUCTIVE_GEOMETRY_REPRESENTATION('supplemental geometry',(#10644,#10645,#10646,#10647,#10648),#46150)");
+            assert_eq!(body[2].get_definition(), "GEOMETRICALLY_BOUNDED_WIREFRAME_SHAPE_REPRESENTATION('nist_ctc_02_asme1-None',(#10532),#46150)");
+            assert_eq!(
+                body[3].get_definition(),
+                "PROPERTY_DEFINITION_REPRESENTATION(#17,#15)"
+            );
+            assert_eq!(
+                body[4].get_definition(),
+                "PROPERTY_DEFINITION_REPRESENTATION(#18,#16)"
+            );
+            assert_eq!(body[5].get_definition(), "REPRESENTATION('',(#19),#46150)");
+            assert_eq!(body[6].get_definition(), "REPRESENTATION('',(#20),#46150)");
+        } else {
+            panic!("Parsed result is not a step file");
+        }
     }
 }
