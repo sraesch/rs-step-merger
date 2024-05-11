3b7e9845ba464eac6dadfd1faa184e44ff6b9628
sascha-raesch <sascha.raesch@threedy.io>
2024-05-11 23:18:28 +0200

    added: test for serializing/deserializing

--- step-merger/src/step/writer.rs
+++ step-merger/src/step/writer.rs
@@ -45,3 +45,34 @@ pub fn write_step<W: Write>(writer: &mut W, step: &StepData, filename: &str) -> 
 
     Ok(())
 }
+
+#[cfg(test)]
+mod test {
+    use std::{io::BufWriter, str::FromStr};
+
+    use crate::step::{self, StepEntry};
+
+    use super::*;
+
+    #[test]
+    fn test_writing_simple() {
+        let data = include_str!("../../../test_data/minimal-structure.stp");
+        let step = step::StepData::from_str(data).unwrap();
+        assert_eq!(step.get_entries().len(), 65);
+
+        let mut serialized_data: Vec<u8> = Vec::new();
+        {
+            let mut writer = BufWriter::new(&mut serialized_data);
+            write_step(&mut writer, &step, "minimal-structure.stp").unwrap();
+        }
+
+        let serialized_data = String::from_utf8(serialized_data).unwrap();
+        let step2 = step::StepData::from_str(&serialized_data).unwrap();
+        assert_eq!(step.get_entries().len(), step2.get_entries().len());
+
+        for (entry1, entry2) in step.get_entries().iter().zip(step2.get_entries().iter()) {
+            assert_eq!(entry1.id, entry2.id);
+            assert_eq!(entry1.definition, entry2.definition);
+        }
+    }
+}
